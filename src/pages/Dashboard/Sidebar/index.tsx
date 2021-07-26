820e49d5c99a6f6f193af5783b5c0ef465bd5832
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-07-26 19:19:42 -0300

    Commit 13 dos arquivos

--- src/pages/Dashboard/Sidebar/index.tsx
+++ src/pages/Dashboard/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import logo from "../../../assets/images/monetus_logo.png";
+import logo from "../../../assets/images/monetus_logo.svg";
+import Home from "../../../assets/images/icon-home.svg";
+import { HTMLAttributes } from 'react';
 
 const Aside = styled.nav`
   background-color: #F5F8FA;
@@ -14,10 +16,40 @@ const Logo = styled.img`
   top: 20px;
 `;
 
+const BotaoHomeEstilizado = styled.button`
+  position: absolute;
+  left: 0;
+  top: 112px;
+  width: 96px;
+  height: 48px;
+  border-left: 4px solid #F06400;
+  border-right: 0;
+  border-top: 0;
+  border-bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: transparent;
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+function BotaoHome(props: HTMLAttributes<HTMLButtonElement>) {
+  return (
+    <BotaoHomeEstilizado type="button">
+      <img src={Home} alt="Home" />
+    </BotaoHomeEstilizado>
+  );
+}
+
 export function Sidebar() {
   return (
     <Aside>
       <Logo src={logo} alt="Monetus Logo" />
+      <BotaoHome />
     </Aside>
   );
 }
